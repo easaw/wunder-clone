3da770425ba45c7f1a514ffc0ebcb489a7c86e7e
Evan Rubinton <evanrubinton@gmail.com>
2014-08-28 17:14:17 -0400

    Refactor

--- app/assets/javascripts/views/lists/lists_index.js
+++ app/assets/javascripts/views/lists/lists_index.js
@@ -1,5 +1,18 @@
-Wunderclone.Views.ListsIndex = Backbone.View.extend({
+Wunderclone.Views.ListsIndex = Backbone.CompositeView.extend({
 
-  template: JST['lists/index']
+  template: JST['lists/index'],
+  
+  
+  initialize: function(){
+    var view = this;
+    this.listenTo(this.collection, "sync remove add change", this.render);
+  },
+  
+  render: function(){
+    var content = this.template({ lists: this.collection });
+    this.$el.html(content);
+    
+    return this;
+  }
 
 });
