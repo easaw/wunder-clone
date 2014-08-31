89fa1d81612f32d9c0b1cfc7777bcd51126b98b8
Evan Rubinton <evanrubinton@gmail.com>
2014-08-30 21:01:25 -0400

    Fix bug with user avatar uploading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:email, :name, :password)
+    params.require(:user).permit(:email, :name, :password, :avatar)
   end
   
 end
