af1302b1de4affb49f8f1fdfb2ffb1a6c75c1898
Evan Rubinton <evanrubinton@gmail.com>
2014-09-15 19:33:43 -0400

    Remove uncecessary spec files

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :task do
+    name { Faker::Lorem.sentence(3) }
+    list_id { rand(1..9)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,7 @@ describe User do
     it 'requires an email' do 
       expect(FactoryGirl.build(:user, email: '')).not_to be_valid
     end
-    it 'requires a password' do 
-      expect(FactoryGirl.build(:user, password: '')).not_to be_valid
-    end
+    
     it 'requires a unique email' do
       FactoryGirl.build(:user, email: 'me@example.com')
       expect(FactoryGirl.build(:user, email: 'me@example.com')).not_to be_valid
